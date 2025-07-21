b311f3400fbe7e171a2ed6fda28c92352842dfe9
adnanmadi417 <uso53dr6i8s4yt0idkq7f6ivj04en8@bots.bitbucket.org>
2025-07-22 00:23:03 +0800

    install JWT to handle auth system in my app

--- members/models.py
+++ members/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    date_of_birth = models.DateField()
