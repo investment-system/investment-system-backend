fc34cd39b88aa439413bf36f4497bed722cc6b36
adnanmadi417 <uso53dr6i8s4yt0idkq7f6ivj04en8@bots.bitbucket.org>
2025-08-18 20:44:15 +0800

    1. removed latest update admin transactions API endpoint for members

--- transactions/models.py
+++ transactions/models.py
@@ -1,7 +1,6 @@
 from django.utils import timezone
 from django.db import models
-from django.conf import settings
-
+from members.models import Member
 
 class Transaction(models.Model):
     SOURCE_TYPE_CHOICES = [
@@ -27,10 +26,9 @@ class Transaction(models.Model):
         ('ewallet', 'E-Wallet'),
     ]
 
-    member = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # must point to your custom User
-
     transaction_id = models.AutoField(primary_key=True)
     transaction_code = models.CharField(max_length=30, unique=True, blank=True, editable=False)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE, null=False, blank=False)
     source_type = models.CharField(max_length=50, choices=SOURCE_TYPE_CHOICES)
     reference_id = models.CharField(max_length=50, blank=True, null=True)
     direction = models.CharField(max_length=15, choices=DIRECTION_CHOICES, default='in')
